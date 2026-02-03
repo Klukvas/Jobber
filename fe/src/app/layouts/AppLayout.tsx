c59d8d37d33e153fd58338349efce6acaa5aafde
Andrii Pavlenko <andrii.pavlenko_ext@zooplus.com>
2026-02-03 17:36:39 +0200

    fix ;ayout

--- fe/src/app/layouts/AppLayout.tsx
+++ fe/src/app/layouts/AppLayout.tsx
@@ -13,7 +13,7 @@ export function AppLayout() {
   return (
     <div className="flex min-h-screen">
       <Sidebar />
-      <div className="flex flex-1 flex-col">
+      <div className="flex min-w-0 flex-1 flex-col">
         <Header />
         <main className="flex-1 overflow-auto p-4 md:p-6">
           <Outlet />
